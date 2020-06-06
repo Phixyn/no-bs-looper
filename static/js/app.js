60847ad454814a790bbef7755b398a8e6de19498
phixyn <phixyn@gmail.com>
2020-06-06 15:24:10 +0100

    :package: Add a two-handle slider for loop time input
    Adds a Foundation two-handle slider that can be used to set
    the start and end times of the portion of the video to loop.
    This can be used in addition to the number input fields. The
    input fields will update a new number of seconds everytime
    the slider is moved, as the slider handles are bound to the
    input fields. Similarly, if a new value is entered in the
    input fields, the slider is updated.
    
    This is very _epico_. Just needs some bug fixing and
    adjustments.

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,25 @@
-$(document).foundation()
+// Initialize all Foundation plugins
+$(document).foundation();
 
 var videoId = document.getElementById("video-id").value;
-var startTime = parseInt(document.getElementById("start-time").value);
-var endTime = parseInt(document.getElementById("end-time").value);
+// var startTime = parseInt(document.getElementById("start-time").value);
+// var endTime = parseInt(document.getElementById("end-time").value);
+var startTime;
+var endTime;
+
+var startTimeInput = document.getElementById("start-time");
+var endTimeInput = document.getElementById("end-time");
+
+var sliderDiv = document.getElementById("loop-portion-slider");
+// TODO Could improve initialization to remove data-* params from HTML <div> element
+var loopPortionSlider = new Foundation.Slider($(sliderDiv));
+var startTimeSliderHandle = document.getElementById("start-time-handle");
+var endTimeSliderHandle = document.getElementById("end-time-handle");
+
+// TODO Debug only
+$(sliderDiv).on("moved.zf.slider", function() {
+  console.log("a");
+});
 
 // Load the IFrame Player API code asynchronously
 var tag = document.createElement('script');
@@ -27,7 +44,7 @@ function onYouTubeIframeAPIReady() {
       modestBranding: 1
     },
     events: {
-      // 'onReady': onPlayerReady,
+      'onReady': onPlayerReady,
       'onStateChange': onPlayerStateChange
     }
   });
@@ -38,8 +55,7 @@ function onYouTubeIframeAPIReady() {
  *
  * Reference: https://developers.google.com/youtube/iframe_api_reference#Events
  */
-// function onPlayerReady(event) {
-  // event.target.playVideo();
+function onPlayerReady(event) {
   // Could use this, but if user seeks, endSeconds becomes invalidated
   // This is the same reason we don't have loop: 1 in our playerVars. Because
   // we have our own loop through the setInterval callback.
@@ -50,7 +66,43 @@ function onYouTubeIframeAPIReady() {
   //     endSeconds: endTime
   //   }
   // );
-// }
+
+  console.log(event);
+  console.log(event.target);
+
+  startTime = parseInt(startTimeInput.value);
+  // player.getDuration() = 1634.781  ... might need to change precision of slider and also data type
+  // For now use parseInt()
+  // ALso maybe use `event.target` instead of `player`
+  endTime = parseInt(player.getDuration());
+
+  // TODO: start of updateSliderAndInputAttributes(endTime);
+  // Just JavaScript things
+  endTimeString = endTime.toString();
+
+  // Update max attribute for time number fields.
+  // Remember both inputs and both handles have to be updated everytime the
+  // video changes, otherwise user might not be able to use text fields to
+  // new times properly and will be forced to use the slider. We want users
+  // to have a choice and for both choices to work 100% all the time.
+  endTimeInput.setAttribute("max", endTimeString);
+  // Don't want start portion slider to be able to go all the way to the end
+  startTimeInput.setAttribute("max", (endTime - 1).toString());
+
+  // By default, we'll put the end slider at the end of video time
+  endTimeInput.value = endTimeString;
+  loopPortionSlider.options.end = endTime;
+
+  // Update slider element with new value for 'data-end' (the max/end value of
+  // the slider).
+  loopPortionSlider.setAttribute("data-end", endTimeString);
+
+  // Update ARIA 'valuemax' data for time slider handles. Entirely for
+  // accessibility purposes, has no effect on handles' functionality.
+  startTimeSliderHandle.setAttribute("aria-valuemax", (endTime - 1).toString());
+  endTimeSliderHandle.setAttribute("aria-valuemax", endTimeString);
+  // TODO: end of updateSliderAndInputAttributes(endTime);
+}
 
 /**
  * Called by the API when the video player's state changes.
@@ -81,6 +133,8 @@ function eventCallback() {
 }
 
 function updatePlayer() {
+  // This has to be changed so user can only submit video ID.
+  // We then need to reset startTime to 0 and endTime to video duration.
   console.debug("Updating player.");
   videoId = document.getElementById("video-id").value;
   startTime = parseInt(document.getElementById("start-time").value);
@@ -96,6 +150,8 @@ function updatePlayer() {
       startSeconds: startTime
     }
   );
+
+  // TODO: updateSliderAndInputAttributes(endTime);
 }
 
 function togglePlayer() {
